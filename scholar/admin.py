24cea23393aca90f7f03c83d14fab046f28dfac8
StefanyG <49376967+StefanyG@users.noreply.github.com>
2020-12-03 15:31:54 -0300

    Add files via upload

---
+++ scholar/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Autor)
+admin.site.register(AutorPaper)
+admin.site.register(Citas)
+admin.site.register(Coautores)
+admin.site.register(Instituciones)
